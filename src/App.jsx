48e17ee24388ef138ee6bd29cd76edd289f824f3
Darsh_Balar <darshbalar2005@gmail.com>
2025-12-15 22:30:33 +0530

    Added Few Pages and Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Menubar from "./components/Menubar";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import PreviewPage from "./pages/PreviewPage";
+import MainPage from "./pages/MainPage";
 
 const App = () => {
-  return <div className="text-amber-700">
-    <img src="../src/logo-svg.svg" alt="" />
-    dasjhb
-  </div>;
+  return (
+    <BrowserRouter>
+      <Menubar />
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/generate" element={<MainPage />} />
+        <Route path="/preview" element={<PreviewPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
